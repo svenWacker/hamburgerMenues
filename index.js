4ecb0e5ac917575eca4161198dfa426447af4494
svenWacker <s.u.wacker@outlook.de>
2021-03-17 13:58:37 +0100

    hamburger menue the third in progress

---
+++ index.js
@@ -0,0 +1,33 @@
+const menuBtn1 = document.querySelector(".burger1");
+let menuOpen1 = false;
+menuBtn1.addEventListener("click", () => {
+  if (!menuOpen1) {
+    menuBtn1.classList.add("open1");
+    menuOpen1 = true;
+  } else {
+    menuBtn1.classList.remove("open1");
+    menuOpen1 = false;
+  }
+});
+const menuBtn2 = document.querySelector(".burger2");
+let menuOpen2 = false;
+menuBtn2.addEventListener("click", () => {
+  if (!menuOpen2) {
+    menuBtn2.classList.add("open2");
+    menuOpen2 = true;
+  } else {
+    menuBtn2.classList.remove("open2");
+    menuOpen2 = false;
+  }
+});
+const menuBtn3 = document.querySelector(".burger3");
+let menuOpen3 = false;
+menuBtn2.addEventListener("click", () => {
+  if (!menuOpen2) {
+    menuBtn3.classList.add("open3");
+    menuOpen3 = true;
+  } else {
+    menuBtn3.classList.remove("open3");
+    menuOpen3 = false;
+  }
+});
